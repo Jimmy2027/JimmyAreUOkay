702dda7b389010e3a6cce1b896df602d36f57bf2
HendrikKlug-synthara <hendrik.klug@synthara.ai>
2021-12-20 17:30:26 +0100

    add norby message for failure

--- tests/test_ppb.py
+++ tests/test_ppb.py
@@ -1,6 +1,7 @@
 import unittest
 from pathlib import Path
 
+import norby
 import ppb
 from ppb.MongoDB import MongoDatabase
 from ppb.utils import get_config
@@ -11,17 +12,23 @@ class TestPPB(unittest.TestCase):
         """
         Test if a connection to the db can be established.
         """
-        config = get_config()
-        ppb_config = config['ppb_config']
-        mongodb = MongoDatabase(mongodb_uri=ppb_config['PPB_TARGET_HOST_DB_URI'])
-        print(list(mongodb.collection.find({})))
+        try:
+            config = get_config()
+            ppb_config = config['ppb_config']
+            mongodb = MongoDatabase(mongodb_uri=ppb_config['PPB_TARGET_HOST_DB_URI'])
+            print(list(mongodb.collection.find({})))
+        except Exception as e:
+            norby.send_msg(whichbot='jimmy_watchdog', message=f'Test test_ppb_db failed with: \n {e}')
 
     def test_upload(self):
         """
         Test if a file can be uploaded.
         """
-        test_file = Path(__file__).parent / 'data/test_file.txt'
-        ppb.upload(file_path=test_file, lifetime=1)
+        try:
+            test_file = Path(__file__).parent / 'data/test_file.txt'
+            ppb.upload(file_path=test_file, lifetime=1)
+        except Exception as e:
+            norby.send_msg(whichbot='jimmy_watchdog', message=f'Test test_upload failed with: \n {e}')
 
 
 if __name__ == '__main__':
