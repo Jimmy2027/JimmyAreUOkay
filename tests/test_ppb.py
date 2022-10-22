84fa9d7de40e4e48d6aa14875b40355ccb57b47a
HendrikKlug-synthara <hendrik.klug@synthara.ai>
2022-10-22 15:44:58 +0200

    convert ppb test to npb test

--- tests/test_ppb.py
+++ tests/test_ppb.py
@@ -2,24 +2,10 @@ import unittest
 from pathlib import Path
 
 import norby
-import ppb
-from ppb.MongoDB import MongoDatabase
-from ppb.utils import get_config
+import npb
 
 
-class TestPPB(unittest.TestCase):
-    def test_ppb_db(self):
-        """
-        Test if a connection to the db can be established.
-        """
-        try:
-            config = get_config()
-            ppb_config = config['ppb_config']
-            mongodb = MongoDatabase(mongodb_uri=ppb_config['PPB_TARGET_HOST_DB_URI'])
-            print(list(mongodb.collection.find({})))
-        except Exception as e:
-            norby.send_msg(whichbot='jimmy_watchdog', message=f'Test test_ppb_db failed with: \n {e}')
-            assert False, e
+class TestNPB(unittest.TestCase):
 
     def test_upload(self):
         """
@@ -27,7 +13,7 @@ class TestPPB(unittest.TestCase):
         """
         try:
             test_file = Path(__file__).parent / 'data/test_file.txt'
-            ppb.upload(file_path=test_file, lifetime=1)
+            npb.upload(file_path=test_file, lifetime=1)
         except Exception as e:
             norby.send_msg(whichbot='jimmy_watchdog', message=f'Test test_upload failed with: \n {e}')
             assert False, e
